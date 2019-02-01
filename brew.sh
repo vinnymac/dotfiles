c210e7742850368a2383023463e71cdd21a13c51
Vincent Taverna <vinny@getlua.com>
2019-02-01 10:51:18 -0500

    Remove failed to find packages

--- brew.sh
+++ brew.sh
@@ -121,7 +121,6 @@ apps=(
   macdown
   slack
   sourcetree
-  sublime-text3
   transmission
   virtualbox
   visual-studio-code
@@ -131,7 +130,7 @@ apps=(
 brew cask install "${apps[@]}"
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package && qlmanage -r
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv webpquicklook suspicious-package && qlmanage -r
 
 # VS Code Symlinks
 ln -s ./.code/settings.json ~/Library/Application\ Support/Code/User/settings.json
